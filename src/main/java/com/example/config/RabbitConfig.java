8b7eb3cd5e52a33266ce8baff01ed30e12c67b70
VitRocket <vitrocket@gmail.com>
2018-08-21 15:52:22 +0300

    Added MongoDB

---
+++ src/main/java/com/example/config/RabbitConfig.java
@@ -0,0 +1,61 @@
+package com.example.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Vitalii on 21.08.18.
+ */
+@Configuration
+public class RabbitConfig {
+
+	private final String queueName;
+	private final String exchange;
+	private final String routingKey;
+
+	public RabbitConfig(@Value("${rocket.rabbitmq.queue}") String queueName,
+						@Value("${rocket.rabbitmq.exchange}") String exchange,
+						@Value("${rocket.rabbitmq.routingkey}") String routingKey) {
+		this.queueName = queueName;
+		this.exchange = exchange;
+		this.routingKey = routingKey;
+	}
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	DirectExchange exchange() {
+		return new DirectExchange(exchange);
+	}
+
+	@Bean
+	Binding binding(Queue queue, DirectExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+	}
+
+	@Bean
+	public MessageConverter jsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jsonMessageConverter());
+		return rabbitTemplate;
+	}
+
+}
